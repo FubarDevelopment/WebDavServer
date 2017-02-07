0eb66cca06758bb7473eb1a20c82eba9117d9653
Mark Junker <m.junker@fubar-dev.de>
2017-02-07 16:34:08 +0100

    Start of locking implementation

---
+++ FubarDev.WebDavServer/Locking/ILockManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using System.Xml.Linq;
+
+using FubarDev.WebDavServer.FileSystem;
+using FubarDev.WebDavServer.Model;
+
+namespace FubarDev.WebDavServer.Locking
+{
+    public interface ILockManager
+    {
+        Task LockAsync(IEntry entry, XElement owner, bool recursive);
+    }
+}
