289dc66f5b4f93bfad790023868f90757724b498
Mark Junker <m.junker@fubar-dev.de>
2017-02-19 18:25:16 +0100

    Utility function to return all "If" headers

---
+++ src/FubarDev.WebDavServer/WebDavRequestHeadersExtensions.cs
@@ -0,0 +1,27 @@
+﻿// <copyright file="WebDavRequestHeadersExtensions.cs" company="Fubar Development Junker">
+// Copyright (c) Fubar Development Junker. All rights reserved.
+// </copyright>
+
+using System.Collections.Generic;
+
+using FubarDev.WebDavServer.Model.Headers;
+
+namespace FubarDev.WebDavServer
+{
+    public static class WebDavRequestHeadersExtensions
+    {
+        public static IEnumerable<IIfMatcher> GetIfHeaderMatchers(this IWebDavRequestHeaders headers)
+        {
+            if (headers.IfMatch != null)
+                yield return headers.IfMatch;
+            if (headers.IfNoneMatch != null)
+                yield return headers.IfNoneMatch;
+            if (headers.IfModifiedSince != null)
+                yield return headers.IfModifiedSince;
+            if (headers.IfUnmodifiedSince != null)
+                yield return headers.IfUnmodifiedSince;
+            if (headers.If != null)
+                yield return headers.If;
+        }
+    }
+}
