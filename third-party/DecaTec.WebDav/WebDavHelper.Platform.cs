a9e11a2fad66a1ed14ea164daf0fda365faec16f
Mark Junker <m.junker@fubar-dev.de>
2017-02-21 14:13:06 +0100

    Use/allow the extension method syntax

--- third-party/DecaTec.WebDav/WebDavHelper.Platform.cs
+++ third-party/DecaTec.WebDav/WebDavHelper.Platform.cs
@@ -13,7 +13,7 @@ namespace DecaTec.WebDav
         /// </summary>
         /// <param name="responseMessage">The <see cref="WebDavResponseMessage"/> whose <see cref="LockToken"/> should be retrieved.</param>
         /// <returns>The <see cref="LockToken"/> of the <see cref="WebDavResponseMessage"/> or null if the WebDavResponseMessage does not contain a lock token.</returns>
-        public static LockToken GetLockTokenFromWebDavResponseMessage(WebDavResponseMessage responseMessage)
+        public static LockToken GetLockTokenFromWebDavResponseMessage(this WebDavResponseMessage responseMessage)
         {
             // Try to get lock token from response header.
             var lockTokenHeaderValue = responseMessage.Headers.GetValues(WebDavRequestHeader.LockTocken).FirstOrDefault();
@@ -24,7 +24,7 @@ namespace DecaTec.WebDav
             // If lock token was not submitted by response header, it should be found in the response content.
             try
             {
-                var prop = WebDavResponseContentParser.ParsePropResponseContentAsync(responseMessage.Content).Result;
+                var prop = responseMessage.Content.ParsePropResponseContentAsync().Result;
                 return new LockToken(prop.LockDiscovery.ActiveLock[0].LockToken.Href);
             }
             catch (Exception)
