502ffc143a7ae39e5f8e9760a3a242052df289b1
Mark Junker <m.junker@fubar-dev.de>
2019-02-01 16:51:06 +0100

    Optional controller (unused yet) required for Win7 WebDAV redirector
    The redirector queries OPTIONS /

---
+++ sample/FubarDev.WebDavServer.Sample.AspNetCore/Controllers/WinRootCompatController.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FubarDev.WebDavServer.Sample.AspNetCore.Controllers
+{
+    /*
+    [Route("")]
+    [Authorize]
+    public class WinRootCompatController : ControllerBase
+    {
+        [HttpOptions]
+        public IActionResult QueryOptions()
+        {
+            return Ok();
+        }
+    }
+    */
+}
