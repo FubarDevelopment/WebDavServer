a3ff954df2511b6b8440975f7302e08e0e03389c
Mark Junker <m.junker@fubar-dev.de>
2019-03-11 17:05:36 +0100

    Enforce using ASP.NET Core 2.2

--- sample/FubarDev.WebDavServer.Sample.AspNetCore/Startup.cs
+++ sample/FubarDev.WebDavServer.Sample.AspNetCore/Startup.cs
@@ -25,6 +25,7 @@ using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
@@ -87,7 +88,7 @@ namespace FubarDev.WebDavServer.Sample.AspNetCore
             services.Configure<WebDavHostOptions>(cfg => Configuration.Bind("Host", cfg));
 
             services
-                .AddMvcCore()
+                .AddMvcCore().SetCompatibilityVersion(CompatibilityVersion.Version_2_2)
                 .AddAuthorization()
                 .AddWebDav();
 
